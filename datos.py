72ebd585d492ca1ea98dd3933465ce6260cd0040
pedrinhobm <137945040+pedrinhobm@users.noreply.github.com>
2023-10-30 19:56:58 -0500

    Update datos.py

--- datos.py
+++ datos.py
@@ -5,6 +5,22 @@ client_socket.send(str(numero_entero).encode())
 # En el servidor
 numero_recibido = int(client_socket.recv(1024).decode())
 
+-----------------------------------------------------------
+# Solicitar al usuario que ingrese un número entero
+num = int(input("Ingrese un número entero: "))
+# Enviar el número al servidor
+client_socket.send(str(num).encode())
+
+# Recibir el número entero desde el cliente
+num = int(client_socket.recv(1024).decode())
+# Calcular el factorial
+fact = factorial(num)
+# Enviar el resultado al cliente
+client_socket.send(str(fact).encode())
+
+# Recibir el resultado del servidor
+resultado = client_socket.recv(1024).decode()
+------------------------------------------------------------
 
 # En el cliente
 numero_decimal = 3.14159
@@ -73,7 +89,7 @@ from datetime import datetime
 fecha_hora_recibida = client_socket.recv(1024).decode()
 fecha_hora_objeto = datetime.strptime(fecha_hora_recibida, "%Y-%m-%d %H:%M:%S")
 
-
+----------------------------------------------------------------------------------
 # Agregar información a la base de datos CSV:
 import csv
 
@@ -89,6 +105,13 @@ with open(archivo_csv, mode='a', newline='') as file:
     
     # Escribe la nueva fila en el archivo CSV
     writer.writerow(nuevo_paciente)
+_____________________________________________________________________________________________
+    # Agregar la información a la base de datos (archivo CSV)
+    with open('pacientes.csv', 'a', newline='') as file:
+        writer = csv.writer(file)
+        paciente_info = data.split(',')
+        writer.writerow(paciente_info)
+------------------------------------------------------------------------------------------------------
 
 # Recuperar información de la base de datos CSV:
 import csv
@@ -103,3 +126,36 @@ with open(archivo_csv, mode='r') as file:
     # Recorre las filas del archivo y muestra los datos
     for row in reader:
         print(f"Paciente: {row['paciente']}, Edad: {row['edad']}, Diagnóstico: {row['diagnostico']}")
+
+
+
+
+
+# Leer las notas desde un archivo CSV y convertirlas a enteros
+def leer_notas():
+    with open("notas.csv", "r") as csvfile:
+        reader = csv.reader(csvfile)
+        notas = next(reader)
+    return [int(nota) for nota in notas]
+
+# Leer y enviar las notas al servidor
+notas = leer_notas()
+client_socket.send(",".join(map(str, notas)).encode())
+
+# Recibir las notas desde el cliente
+notas_str = client_socket.recv(1024).decode()
+notas = [int(nota) for nota in notas_str.split(",")]
+
+# envia
+ _, _, pacientes_sanos = calcular_estadisticas()
+client_socket.send(str(pacientes_sanos).encode())
+# recive
+resultado = client_socket.recv(1024).decode()
+print("Resultado:", resultado)
+
+
+# Solicitar estadísticas al servidor
+opcion = input("Seleccione una opción (a, b, o c): ")
+client_socket.send(opcion.encode())
+
+opcion = client_socket.recv(1).decode()
